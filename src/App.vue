a7501752302b3801d28a4be6460b82a53cca5f19
Carlos Torres <torresmeneses888@gmail.com>
2024-03-19 10:20:42 -0600

    ⭐ Feat: Resolucion de la evaluacion

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@ const shoppingIcon = ref('material-icons shopping-cart-icon');
 //Creando una referencia reactiva
 //para almacenar el valor de la lista
 const items = ref([
-{id: 0, label:'leche'},
-{id: 1, label:'Arroz'},
-{id: 2, label:'Carne'},
-{id: 3, label:'Pan'},
-{id: 4, label:'Huevos'}
+// {id: 0, label:'leche'},
+// {id: 1, label:'Arroz'},
+// {id: 2, label:'Carne'},
+// {id: 3, label:'Pan'},
+// {id: 4, label:'Huevos'}
   ]);
   const newItem = ref('');
   const newItemHighPriority = ref(false);
@@ -24,20 +24,40 @@ const items = ref([
   //  Borrar el contenido de la caja de texto
   newItem.value="";
   };
+  const editing = ref (false);
+  const doEdit = (edit) =>{
+    editing.value = edit,
+    newItem.value ="";
+  }
+
 </script>
 
 <template>
-  <h1><i :class="shoppingIcon">local_mall</i> {{ header }} </h1>
-  <form v-on:submit.prevent="saveItems" class="add-item form">
+  <!-- Header -->
+  <div>
+  <h1>
+    <i :class="shoppingIcon">local_mall</i> {{ header }} 
+  </h1>
+  <button class="btn" v-if="editing" v-on:click="doEdit(false)">Cancelar</button>
+  <button class="btn btn-primary" v-if="!editing" v-on:click="doEdit(true)">Agregar articulos</button>
+</div>
+  <!-- Formulario -->
+  <form 
+  v-if="editing"
+  v-on:submit.prevent="saveItems" class="add-item form">
   <input 
-  v-model="newItem" type="text"placeholder="Agregar articulo">
-  <label ><input type="checkbox" v-model="newItemHighPriority">Alta prioridad</label>
+  v-model="newItem" type="text"
+  placeholder="Agregar articulo">
   <!-- boton -->
+  <label ><input type="checkbox" v-model="newItemHighPriority">Alta prioridad</label>
   <button class="btn btn-primary">Agregar articulo</button>
   </form>
+  <!-- Entrega de lista -->
   <ul>
     <li v-for="{id, label} in items" v-bind:key="id">⭐{{ label }}</li>
   </ul>
+  <!-- Mensaje condicional -->
+  <p v-if="items.length === 0">🥀No hay elementos en la lista🥀</p>
 </template>
 
 <style scoped>
